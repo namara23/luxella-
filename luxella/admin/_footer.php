338cab24a0558c2e58ad62fd5bf5ef26fc571c6f
namara23 <namarajoshua97@gmail.com>
2026-06-08 16:04:55 +0300

    Delete luxella directory

--- luxella/admin/_footer.php
+++
@@ -1,1 +0,0 @@
-</div></main></body></html>
--- luxella/admin/logout.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once __DIR__ . '/../includes/config.php';
-$_SESSION = []; session_destroy();
-header('Location: ' . url('admin/login.php')); exit;
